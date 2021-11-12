b3c11f79258cfc1e57fc34c786fbdbf8fcae3f6a
DenjanD <alfien311@gmail.com>
2021-11-12 17:12:54 +0700

    Commit awal

---
+++ P2_1_1204039/Book_1204039.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_1_1204039
+{
+    public class Book_1204039: Product_1204039
+    {
+        protected string pageCount;
+
+        public Book_1204039(string type, string title, string pagecount) : base(type, title)
+        {
+            this.pageCount = pagecount;
+        }
+
+        public string PageCount
+        {
+            get
+            {
+                return pageCount;
+            }
+
+            set
+            {
+                pageCount = value;
+            }
+        }
+    }
+}
---
+++ P2_1_1204039/Product_1204039.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace P2_1_1204039
+{
+    public class Product_1204039
+    {
+        private String myType;
+        private String myTitle;
+
+        public Product_1204039()
+        {
+
+        }
+
+        public Product_1204039(string type, string title)
+        {
+            this.myType = type;
+            this.myTitle = title;
+        }
+
+        public string MyType
+        {
+            get
+            {
+                return myType;
+            }
+
+            set
+            {
+                myType = value;
+            }
+        }
+
+        public string MyTitle
+        {
+            get
+            {
+                return myTitle;
+            }
+
+            set
+            {
+                myTitle = value;
+            }
+        }
+    }
+}
